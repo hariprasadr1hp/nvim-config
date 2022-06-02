a5ee31c3723f303dbd8227216f97ecf29c0fe261
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2022-06-02 13:45:40 +0200

    packer-setup update

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,38 +1,12 @@
-local execute = vim.api.nvim_command
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-
-
 if fn.empty(fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
-  execute 'packadd packer.nvim'
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
-
---- Check if a file or directory exists in this path
-local function require_plugin(plugin)
-    local plugin_prefix = fn.stdpath("data") .. "/site/pack/packer/opt/"
-
-    local plugin_path = plugin_prefix .. plugin .. "/"
-    --	print('test '..plugin_path)
-    local ok, err, code = os.rename(plugin_path, plugin_path)
-    if not ok then
-        if code == 13 then
-            -- Permission denied, but it exists
-            return true
-        end
-    end
-    --	print(ok, err, code)
-    if ok then
-        vim.cmd("packadd " .. plugin)
-    end
-    return ok, err, code
-end
-
-vim.cmd "autocmd BufWritePost plugins.lua PackerCompile" -- Auto compile when there are changes in plugins.lua
 
 
 return require('packer').startup(
-	function()
+	function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
@@ -145,5 +119,8 @@ return require('packer').startup(
 
     -- -- Interactive code
     -- use 'metakirby5/codi.vim'
+	if packer_bootstrap then
+		require('packer').sync()
+	end
 
 end)
