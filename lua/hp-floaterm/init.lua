6523310bb6ff29facd15bfb342314d46e375c2e0
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2024-03-17 20:11:27 +0100

    keybinding for terminal toggle

--- lua/hp-floaterm/init.lua
+++ lua/hp-floaterm/init.lua
@@ -13,6 +13,8 @@ vim.g.floaterm_width=0.8
 vim.g.floaterm_height=0.8
 vim.g.floaterm_wintitle=0
 vim.g.floaterm_autoclose=1
+vim.g.floaterm_opener='edit'
+vim.g.floaterm_keymap_toggle = '<Leader>ot'
 
 
 require("toggleterm").setup{}
