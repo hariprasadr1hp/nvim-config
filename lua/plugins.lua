3738f83636b52769c8d5fb9510aa950133966d02
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2024-06-22 19:50:16 +0200

    [fix] treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -50,8 +50,13 @@ return require('packer').startup(
 	}
 
 	-- treesitter
-	use {"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"}
-	use {"windwp/nvim-ts-autotag", opt = true}
+	use {
+		'nvim-treesitter/nvim-treesitter',
+        run = function()
+            local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+            ts_update()
+        end,
+    }
 	use 'nvim-treesitter/playground'
 
 	-- telescope
