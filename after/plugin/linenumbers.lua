f7b497625d5c6ed9dd4e7c511ca3adeec27136d7
hariprasadr1hp <hariprasadr1hp@tutanota.com>
2024-10-20 18:52:57 +0200

    [fix] consitent colors during recording mode
    - the colors of linenumbers, while on recording mode, should persist - -
    - should be mode agnostic

--- after/plugin/linenumbers.lua
+++ after/plugin/linenumbers.lua
@@ -1,60 +1,93 @@
 -- after/plugin/linenumbers.lua
 
+local is_recording = require("helpers").is_recording
+
 local colors = {
 	red = "#e06c75",
 	yellowish_green = "#98c379",
 	green = "#71b951",
 	yellow = "#e5c07b",
 	blue = "#0987ff",
+	purple = "#c678dd",
+	white = "#ffffff",
+	black = "#000000",
 }
+
+local function set_normal_mode_colors()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red, bold = true })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.yellowish_green })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.green })
+end
+
+local function set_insert_mode_colors()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red, bold = true })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.yellow })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.yellow })
+end
+
+local function set_replace_mode_colors()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red, bold = true })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.white })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.white })
+end
+
+local function set_terminal_mode_colors()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red, bold = true })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.purple })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.purple })
+end
+
+local function set_cmdline_mode_colors()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red, bold = true })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.black })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.black })
+end
+
+local function set_visual_mode_colors()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.yellowish_green, bold = true })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.red })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.red })
+end
+
+local function set_while_recording_colors()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = colors.blue, bold = true })
+	vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.blue })
+	vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.blue })
+end
 
 vim.api.nvim_create_autocmd("ColorScheme", {
 	pattern = "*",
-	callback = function()
-		vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red })
-		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.yellowish_green })
-		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.green })
-	end,
+	callback = set_normal_mode_colors,
 })
 
 vim.api.nvim_create_autocmd("ModeChanged", {
 	pattern = "*",
 	callback = function()
 		local mode = vim.fn.mode()
-		if mode == "n" then
-			vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red })
-			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.yellowish_green })
-			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.green })
+		if is_recording() then
+			set_while_recording_colors()
+		elseif mode == "n" then
+			set_normal_mode_colors()
 		elseif mode == "i" then
-			vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red })
-			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.yellow })
-			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.yellow })
+			set_insert_mode_colors()
+		elseif mode == "t" then
+			set_terminal_mode_colors()
+		elseif mode == "R" then
+			set_replace_mode_colors()
 		elseif mode == "v" or "V" then
-			vim.api.nvim_set_hl(0, "LineNr", { fg = colors.yellowish_green })
-			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.red })
-			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.red })
+			set_visual_mode_colors()
 		else
-			vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red })
-			vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.yellowish_green })
-			vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.green })
+			set_normal_mode_colors()
 		end
 	end,
 })
 
 vim.api.nvim_create_autocmd("RecordingEnter", {
 	pattern = "*",
-	callback = function()
-		vim.api.nvim_set_hl(0, "LineNr", { fg = colors.blue })
-		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.blue })
-		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.blue })
-	end,
+	callback = set_while_recording_colors,
 })
 
 vim.api.nvim_create_autocmd("RecordingLeave", {
 	pattern = "*",
-	callback = function()
-		vim.api.nvim_set_hl(0, "LineNr", { fg = colors.red })
-		vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.yellowish_green })
-		vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.green })
-	end,
+	callback = set_normal_mode_colors,
 })
